17af31508ae8e954b04351d790f8a4287f9ad8a1
AlbeCamp21 <luis.alanya.c@uni.pe>
2025-07-14 14:34:48 -0500

    feat: agregar estructura básica para script/make_report.sh

--- scripts/make_report.sh
+++ scripts/make_report.sh
@@ -1,1 +1,24 @@
 #!/bin/bash
+
+# ejecuta el script graph_analysis.py y el output se almacena en metrics.json
+step_init(){
+    python src/graph_analysis.py --output metrics.json;
+}
+
+# ejecuta el script report_suite.py y genera el reporte en el archivo report.md
+step_report(){
+    python src/report_suite.py --format md --output metrics.json --output report.md;
+}
+
+# muestra el archivo report.md
+step_preview(){
+    less report.md;
+}
+
+main(){
+    step_init
+    step_report
+    step_preview
+}
+
+main
